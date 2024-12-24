ee6541371c4f8197b98cbdfa0f7ffe91db98e813
iiakshat <jakshat569@gmail.com>
2024-12-24 21:34:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,7 @@ class SimpleNeuralNetwork:
 
     def relu(self, x):
         return np.maximum(0, x)
-    
+        
+    def softmax(self, x):
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
